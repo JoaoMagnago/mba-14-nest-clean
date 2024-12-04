6831c5323ebd7dd389946e254676c0e6ac6f03d8
joao magnago <joaomagnago.wm@gmail.com>
2024-12-03 23:37:40 -0300

    feat: add create question controller to app and use auth guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
-import { AuthenticateController } from './controllers/authenticate-controller'
+import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
+import { CreateQuestionController } from './controllers/create-question.controller'
 import { envSchema } from './env'
 import { PrismaService } from './prisma/prisma.service'
 
@@ -12,7 +13,7 @@ import { PrismaService } from './prisma/prisma.service'
     isGlobal: true
   }),
     AuthModule],
-  controllers: [CreateAccountController, AuthenticateController],
+  controllers: [CreateAccountController, AuthenticateController, CreateQuestionController],
   providers: [PrismaService],
 })
 export class AppModule { }
