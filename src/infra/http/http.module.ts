f77fbb7c00e1f3fb6b05fcc3694dde54058c0d8d
joao magnago <joaomagnago.wm@gmail.com>
2024-12-07 17:50:13 -0300

    feat(question): complete flow of fetch recent questions use case

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,4 +1,5 @@
 import { CreateQuestionUseCase } from "@/domain/forum/application/use-cases/create-question";
+import { FetchRecentQuestionsUseCase } from "@/domain/forum/application/use-cases/fetch-recent-questions";
 import { Module } from "@nestjs/common";
 import { DatabaseModule } from "../database/database.module";
 import { AuthenticateController } from "./controllers/authenticate.controller";
@@ -8,9 +9,15 @@ import { FetchRecentQuestionsController } from "./controllers/fetch-recent-quest
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [CreateAccountController, AuthenticateController, CreateQuestionController, FetchRecentQuestionsController],
-  providers: [CreateQuestionUseCase]
+  controllers: [
+    CreateAccountController,
+    AuthenticateController,
+    CreateQuestionController,
+    FetchRecentQuestionsController
+  ],
+  providers: [
+    CreateQuestionUseCase,
+    FetchRecentQuestionsUseCase
+  ]
 })
-export class HttpModule {
-
-}
+export class HttpModule { }
