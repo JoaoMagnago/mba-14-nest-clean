8f16c43a4e682f9d79c81e256ab425e3ff0bf630
joao magnago <joaomagnago.wm@gmail.com>
2024-12-07 19:24:22 -0300

    feat: create cryptography gateways

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -3,12 +3,26 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 interface StudentProps {
   name: string
+  email: string
+  password: string
 }
 
 export class Student extends Entity<StudentProps> {
-  static create(props: StudentProps, id?: UniqueEntityID) {
-    const answer = new Student(props, id)
+  get name() {
+    return this.props.name
+  }
 
-    return answer
+  get email() {
+    return this.props.email
+  }
+
+  get password() {
+    return this.props.password
+  }
+
+  static create(props: StudentProps, id?: UniqueEntityID) {
+    const student = new Student(props, id)
+
+    return student
   }
 }
