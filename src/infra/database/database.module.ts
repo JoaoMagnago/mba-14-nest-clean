9a6742e5703367adb81118183ad8b4028d4fc6d7
joao magnago <joaomagnago.wm@gmail.com>
2024-12-08 00:39:12 -0300

    feat: write remaining prisma repositories

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -5,8 +5,8 @@ import { PrismaService } from "./prisma/prisma.service";
 import { PrismaAnswerAttachmentsRepository } from "./prisma/repositories/prisma-answer-attachments-repository";
 import { PrismaAnswerCommentsRepository } from "./prisma/repositories/prisma-answer-comments-repository";
 import { PrismaAnswersRepository } from "./prisma/repositories/prisma-answers-repository";
-import { PrismaQuestionCommentsRepository } from "./prisma/repositories/prisma-question-comments";
-import { PrismaQuestionAttachmentsRepository } from "./prisma/repositories/prisma-question.attachment";
+import { PrismaQuestionCommentsRepository } from "./prisma/repositories/prisma-question-comments-repository";
+import { PrismaQuestionAttachmentsRepository } from "./prisma/repositories/prisma-question.attachments-repository";
 import { PrismaQuestionsRepository } from "./prisma/repositories/prisma-questions-repository";
 import { PrismaStudentsRepository } from "./prisma/repositories/prisma-students-repository";
 
