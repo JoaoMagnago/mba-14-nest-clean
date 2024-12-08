058bd7d2af47c8c181d2cdae7d913e9c48e5525e
joao magnago <joaomagnago.wm@gmail.com>
2024-12-07 21:16:50 -0300

    feat(http): use nest public auth approach in controllers

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -1,12 +1,10 @@
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
-import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard'
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
 import {
   BadRequestException,
   Controller,
   Get,
-  Query,
-  UseGuards
+  Query
 } from '@nestjs/common'
 import { z } from 'zod'
 import { QuestionPresenter } from '../presenters/question-presenter'
@@ -23,7 +21,6 @@ type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
 const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
 
 @Controller('/questions')
-@UseGuards(JwtAuthGuard)
 export class FetchRecentQuestionsController {
   constructor(
     private fetchRecentQuestions: FetchRecentQuestionsUseCase
