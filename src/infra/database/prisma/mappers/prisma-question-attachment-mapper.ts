4b911932dfab3f9c2dbaa365028b34b6c8156f2f
joao magnago <joaomagnago.wm@gmail.com>
2024-12-09 20:50:28 -0300

    feat(question): include attachments in controller

--- src/infra/database/prisma/mappers/prisma-question-attachment-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question-attachment-mapper.ts
@@ -1,6 +1,6 @@
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { QuestionAttachment } from "@/domain/forum/enterprise/entities/question-attachment";
-import { Attachment as PrismaAttachment } from "@prisma/client";
+import { Prisma, Attachment as PrismaAttachment } from "@prisma/client";
 
 
 export class PrismaQuestionAttachmentMapper {
@@ -13,5 +13,24 @@ export class PrismaQuestionAttachmentMapper {
       attachmentId: new UniqueEntityID(raw.id),
       questionId: new UniqueEntityID(raw.questionId),
     }, new UniqueEntityID(raw.id))
+  }
+
+  static toPrismaUpdateMany(
+    attachments: QuestionAttachment[]
+  ): Prisma.AttachmentUpdateManyArgs {
+    const attachmentsIds = attachments.map(attachment => {
+      return attachment.id.toString()
+    })
+
+    return {
+      where: {
+        id: {
+          in: attachmentsIds,
+        },
+      },
+      data: {
+        questionId: attachments[0].questionId.toString(),
+      },
+    }
   }
 }
--- src/infra/database/prisma/repositories/prisma-question.attachments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-question.attachments-repository.ts
@@ -7,12 +7,32 @@ import { PrismaService } from "../prisma.service";
 @Injectable()
 export class PrismaQuestionAttachmentsRepository implements QuestionAttachmentsRepository {
   constructor(private prisma: PrismaService) { }
-  createMany(attachments: QuestionAttachment[]): Promise<void> {
-    throw new Error("Method not implemented.");
+  async createMany(attachments: QuestionAttachment[]): Promise<void> {
+    if (attachments.length === 0) {
+      return
+    }
+
+    const data = PrismaQuestionAttachmentMapper.toPrismaUpdateMany(attachments)
+
+    await this.prisma.attachment.updateMany(data)
   }
 
-  deleteMany(attachments: QuestionAttachment[]): Promise<void> {
-    throw new Error("Method not implemented.");
+  async deleteMany(attachments: QuestionAttachment[]): Promise<void> {
+    if (attachments.length === 0) {
+      return
+    }
+
+    const attachmentsIds = attachments.map(attachment => {
+      return attachment.id.toString()
+    })
+
+    await this.prisma.attachment.deleteMany({
+      where: {
+        id: {
+          in: attachmentsIds
+        }
+      }
+    })
   }
 
   async findManyByQuestionId(questionId: string): Promise<QuestionAttachment[]> {
