d430d6d5ef9a1fc3a14e5d7e2770e5fcc3085a9a
joao magnago <joaomagnago.wm@gmail.com>
2024-12-08 21:31:23 -0300

    feat(attachment): create controller and test for upload attachment use case

---
+++ src/infra/http/controllers/upload-attachment.controller.ts
@@ -0,0 +1,36 @@
+import {
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common'
+import { FileInterceptor } from '@nestjs/platform-express'
+
+@Controller('/attachments')
+export class UploadAttachmentController {
+  // constructor() {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  async handle(
+    @UploadedFile(
+      // If the following classes show errors, update nestjs to 10.2.5
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({
+            maxSize: 1024 * 1024 * 2, // 2mb
+          }),
+          new FileTypeValidator({
+            fileType: '.(png|jpg|jpeg|pdf)',
+          }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    console.log(file)
+  }
+}
