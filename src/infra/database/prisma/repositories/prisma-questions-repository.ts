79885dd1d5efbc6d4085f6ebea9bc4c4d9c5a24c
joao magnago <joaomagnago.wm@gmail.com>
2024-12-11 20:33:19 -0300

    refactor(question): add question details to remaining controllers and tests

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -1,65 +1,25 @@
-import { PaginationParams } from "@/core/repositories/pagination-params";
-import { QuestionAttachmentsRepository } from "@/domain/forum/application/repositories/question-attachments-repository";
-import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
-import { Question } from "@/domain/forum/enterprise/entities/question";
-import { QuestionDetails } from "@/domain/forum/enterprise/entities/value-objects/question-details";
-import { Injectable } from "@nestjs/common";
-import { PrismaQuestionMapper } from "../mappers/prisma-question-mapper";
-import { PrismaService } from "../prisma.service";
+import { PaginationParams } from '@/core/repositories/pagination-params'
+import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { Question } from '@/domain/forum/enterprise/entities/question'
+import { QuestionDetails } from '@/domain/forum/enterprise/entities/value-objects/question-details'
+import { Injectable } from '@nestjs/common'
+import { PrismaQuestionDetailsMapper } from '../mappers/prisma-question-details-mapper'
+import { PrismaQuestionMapper } from '../mappers/prisma-question-mapper'
+import { PrismaService } from '../prisma.service'
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionsRepository {
   constructor(
     private prisma: PrismaService,
-    private questionAttachmentsRepository: QuestionAttachmentsRepository
+    private questionAttachmentsRepository: QuestionAttachmentsRepository,
   ) { }
-
-  async create(question: Question): Promise<void> {
-    const data = PrismaQuestionMapper.toPrisma(question)
-
-    await this.prisma.question.create({
-      data,
-    })
-
-    await this.questionAttachmentsRepository.createMany(
-      question.attachments.getItems()
-    )
-  }
-
-  async save(question: Question): Promise<void> {
-    const data = PrismaQuestionMapper.toPrisma(question)
-
-    await Promise.all([
-      this.prisma.question.update({
-        where: {
-          id: data.id,
-        },
-        data
-      }),
-      this.questionAttachmentsRepository.createMany(
-        question.attachments.getNewItems()
-      ),
-      this.questionAttachmentsRepository.deleteMany(
-        question.attachments.getRemovedItems()
-      )
-    ])
-  }
-
-  async delete(question: Question): Promise<void> {
-    const data = PrismaQuestionMapper.toPrisma(question)
-
-    await this.prisma.question.delete({
-      where: {
-        id: data.id,
-      },
-    })
-  }
 
   async findById(id: string): Promise<Question | null> {
     const question = await this.prisma.question.findUnique({
       where: {
-        id
-      }
+        id,
+      },
     })
 
     if (!question) {
@@ -72,8 +32,8 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
   async findBySlug(slug: string): Promise<Question | null> {
     const question = await this.prisma.question.findUnique({
       where: {
-        slug
-      }
+        slug,
+      },
     })
 
     if (!question) {
@@ -84,7 +44,21 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
   }
 
   async findDetailsBySlug(slug: string): Promise<QuestionDetails | null> {
-    throw new Error("Method not implemented.");
+    const question = await this.prisma.question.findUnique({
+      where: {
+        slug,
+      },
+      include: {
+        author: true,
+        attachments: true,
+      },
+    })
+
+    if (!question) {
+      return null
+    }
+
+    return PrismaQuestionDetailsMapper.toDomain(question)
   }
 
   async findManyRecent({ page }: PaginationParams): Promise<Question[]> {
@@ -93,9 +67,50 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
         createdAt: 'desc',
       },
       take: 20,
-      skip: (page - 1) * 20
+      skip: (page - 1) * 20,
     })
 
     return questions.map(PrismaQuestionMapper.toDomain)
   }
-} 
+
+  async create(question: Question): Promise<void> {
+    const data = PrismaQuestionMapper.toPrisma(question)
+
+    await this.prisma.question.create({
+      data,
+    })
+
+    await this.questionAttachmentsRepository.createMany(
+      question.attachments.getItems(),
+    )
+  }
+
+  async save(question: Question): Promise<void> {
+    const data = PrismaQuestionMapper.toPrisma(question)
+
+    await Promise.all([
+      this.prisma.question.update({
+        where: {
+          id: question.id.toString(),
+        },
+        data,
+      }),
+      this.questionAttachmentsRepository.createMany(
+        question.attachments.getNewItems(),
+      ),
+      this.questionAttachmentsRepository.deleteMany(
+        question.attachments.getRemovedItems(),
+      ),
+    ])
+  }
+
+  async delete(question: Question): Promise<void> {
+    const data = PrismaQuestionMapper.toPrisma(question)
+
+    await this.prisma.question.delete({
+      where: {
+        id: data.id,
+      },
+    })
+  }
+}
--- src/infra/http/controllers/get-question-by-slug.controller.ts
+++ src/infra/http/controllers/get-question-by-slug.controller.ts
@@ -1,30 +1,21 @@
 import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug'
-import {
-  BadRequestException,
-  Controller,
-  Get,
-  Param
-} from '@nestjs/common'
-import { QuestionPresenter } from '../presenters/question-presenter'
+import { BadRequestException, Controller, Get, Param } from '@nestjs/common'
+import { QuestionDetailsPresenter } from '../presenters/question-details-presenter'
 
 @Controller('/questions/:slug')
 export class GetQuestionBySlugController {
-  constructor(
-    private getQuestionBySlug: GetQuestionBySlugUseCase
-  ) { }
+  constructor(private getQuestionBySlug: GetQuestionBySlugUseCase) { }
 
   @Get()
-  async handle(
-    @Param('slug') slug: string
-  ) {
+  async handle(@Param('slug') slug: string) {
     const result = await this.getQuestionBySlug.execute({
-      slug
+      slug,
     })
 
     if (result.isLeft()) {
       throw new BadRequestException()
     }
 
-    return { question: QuestionPresenter.toHTTP(result.value.question) }
+    return { question: QuestionDetailsPresenter.toHTTP(result.value.question) }
   }
 }
