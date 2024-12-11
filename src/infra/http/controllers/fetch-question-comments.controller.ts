1c489db1304e06bbd30030902e64885f376a8de1
joao magnago <joaomagnago.wm@gmail.com>
2024-12-10 21:32:11 -0300

    refactor(comment): add comment with author value object to fetch question comments controller

--- src/infra/http/controllers/fetch-question-comments.controller.ts
+++ src/infra/http/controllers/fetch-question-comments.controller.ts
@@ -5,10 +5,10 @@ import {
   Controller,
   Get,
   Param,
-  Query
+  Query,
 } from '@nestjs/common'
 import { z } from 'zod'
-import { CommentPresenter } from '../presenters/comment-presenter'
+import { CommentWithAuthorPresenter } from '../presenters/comment-with-author-presenter'
 
 const pageQueryParamSchema = z
   .string()
@@ -17,32 +17,30 @@ const pageQueryParamSchema = z
   .transform(Number)
   .pipe(z.number().min(1))
 
-type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
-
 const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
 
 @Controller('/questions/:questionId/comments')
 export class FetchQuestionCommentsController {
-  constructor(
-    private fetchQuestionComments: FetchQuestionCommentsUseCase
-  ) { }
+  constructor(private fetchQuestionComments: FetchQuestionCommentsUseCase) { }
 
   @Get()
   async handle(
     @Query('page', queryValidationPipe) page: PageQueryParamSchema,
-    @Param('questionId') questionId: string
+    @Param('questionId') questionId: string,
   ) {
     const result = await this.fetchQuestionComments.execute({
       page,
-      questionId
+      questionId,
     })
 
     if (result.isLeft()) {
       throw new BadRequestException()
     }
 
-    const questionComments = result.value.questionComments
+    const comments = result.value.comments
 
-    return { comments: questionComments.map(CommentPresenter.toHTTP) }
+    return { comments: comments.map(CommentWithAuthorPresenter.toHTTP) }
   }
 }
